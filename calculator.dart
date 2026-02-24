82c632ae58a2ed71c829f428a2542a429fd28ce0
Piyush Kumar <piyushchilling@gamil.com>
2026-02-24 12:12:36 +0530

    today commit

---
+++ calculator.dart
@@ -0,0 +1,43 @@
+import 'dart:io';
+
+void main() {
+  print("Enter first number:");
+  double num1 = double.parse(stdin.readLineSync()!);
+
+  print("Enter operator (+, -, *, /):");
+  String op = stdin.readLineSync()!;
+
+  print("Enter second number:");
+  double num2 = double.parse(stdin.readLineSync()!);
+
+  double result;
+
+  switch (op) {
+    case '+':
+      result = num1 + num2;
+      print("Result: $result");
+      break;
+
+    case '-':
+      result = num1 - num2;
+      print("Result: $result");
+      break;
+
+    case '*':
+      result = num1 * num2;
+      print("Result: $result");
+      break;
+
+    case '/':
+      if (num2 == 0) {
+        print("Error: Cannot divide by zero.");
+      } else {
+        result = num1 / num2;
+        print("Result: $result");
+      }
+      break;
+
+    default:
+      print("Invalid operator");
+  }
+}
